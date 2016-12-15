160201f39bb853f1281c905b72603d5446801553
Maxilag <guillaume.seyer@edu.itescia.fr>
2016-12-15 12:07:02 +0100

    destruction par fragment

---
+++ Assets/MyScript/TriggerTrue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTrue : MonoBehaviour {
+    public GameObject sphere;
+    private double chrono;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void Update()
+    {
+        chrono += Time.deltaTime;
+        if (chrono > 0.3)
+        {
+            sphere.transform.position = this.gameObject.transform.GetChild(0).gameObject.transform.position;
+            //nombreEnfant++;
+            chrono = 0;
+        }
+    }
+}
